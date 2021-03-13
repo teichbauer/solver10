4c85c7d2cbd58a3c7db5d58f0bca57ecd8b6cc97
Raymond Wei <raymondwei@outlook.com>
2021-03-13 00:30:15 -0600

    2021-03-13->0.151 - progressing on satmgr

--- candinode.py
+++ candinode.py
@@ -2,10 +2,15 @@ class CandiNode:
     def __init__(self, smgr, snode, val, prv_cnode=None):
         self.smgr = smgr
         self.val = val
-        self.tnode = snode.chdic[val]['tnode']
-        self.pvs = snode.chdic[val]['parent-ch-keys'][:]
         self.prv = prv_cnode
-        self.cur = 0
+        self.psnode = snode.parent
+        self.ch = snode.chdic[val]
+        self.tnode = self.ch['tnode']
+        self.reset()  # reset self.pvs=full, and self.next==None
+
+    def reset(self):
+        if self.psnode != None:
+            self.pvs = self.ch['parent-ch-keys'][:]
         self.next = None
 
     def find_next(self):
@@ -15,5 +20,21 @@ class CandiNode:
         pchdic = self.tnode.holder.parent.chdic
         pv = self.p
 
-    def find_candi(self):
-        pass
+    def find_candi(self, sat_array=None):
+        if sat_array == None:
+            sat_array = []
+
+        succ = True
+        for sat in sat_array[1:]:
+            if not self.tnode.check_sat(sat):
+                return None
+
+        sat_array.insert(0, self.ch['hsat'])
+
+        if self.psnode == None:
+            return sat_array
+
+        pv = self.pvs.pop(0)
+        if self.next == None:
+            self.next = CandiNode(self.smgr, self.psnode, pv, self)
+        return self.next.find_candi(sat_array)
